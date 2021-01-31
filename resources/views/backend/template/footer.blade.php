c6b443ba84f433172f437c1c664686baad47b16e
Gahasa Purba <59984259+GahasaPurba@users.noreply.github.com>
2021-01-31 19:55:29 +0700

    Update footer.blade.php

--- resources/views/backend/template/footer.blade.php
+++ resources/views/backend/template/footer.blade.php
@@ -26,7 +26,7 @@
                 <ul class="nav">
                     <li class="nav-item">
                         <a href="https://www.del.ac.id/" target="_blank" class="nav-link">
-                            &copy; 2021 Kelompok 03 PA 1 - IT Del
+                            &copy; <script>document.write(new Date().getFullYear());</script> Kelompok 03 PA 1 - IT Del
                         </a>
                     </li>
                 </ul>
